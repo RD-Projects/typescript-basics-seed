61f22c109ac9a92bd9535dc7d7d14bcf410b1985
Rhyan Rambhadjan <rhyan.rambhadjan@politie.nl>
2021-07-15 16:48:54 +0200

    course changes

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log('Hello TypeScript!');
+class ShoppingList {
+    groceries: string[];
+    constructor(){
+        this.groceries = []
+    }
+    addItem(item: string) {
+        this.groceries.push(item);
+    }
+    removeItem(item: string){
+        this.groceries = this.groceries.filter((grocery) => item !== grocery);
+    }
+}
+
+const myNewList = new ShoppingList()
+myNewList.addItem('kwark')
+myNewList.addItem('sap')
+myNewList.addItem('sla');
+myNewList.addItem('koek')
+console.log(myNewList.groceries)
+myNewList.removeItem('sap');
+console.log(myNewList)
